1adc6740c914176af0689f7768a7f5dbf7bb26e7
Kimboo <kimboo@mail.ru>
2022-10-21 16:52:03 +0600

    asd

--- test.js
+++ test.js
@@ -19,16 +19,16 @@ let currentUser = undefined
 
 const auth = basicAuth({
   authorizer: async (username, password, cb) => {
-   currentUser = await User.findOne({login: username }).exec()
-   const userMatches = basicAuth.safeCompare(username, currentUser.login)
-   const passwordMatches = basicAuth.safeCompare(password, currentUser.password)
-   if (userMatches & passwordMatches)
-       return cb(null, true)
+    currentUser = await User.findOne({login: username }).exec()
+    const userMatches = basicAuth.safeCompare(username, currentUser.login)
+    const passwordMatches = basicAuth.safeCompare(password, currentUser.password)
+    if (userMatches & passwordMatches)
+      return cb(null, true)
     else
-       return cb(null, false)
+      return cb(null, false)
   },
     authorizeAsync: true,
-  })
+})
 
 app.get('/api/token', auth, async (request, responce) => {
     let params = {...request.body}
