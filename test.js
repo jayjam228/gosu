b7c8edbc97584922004308d68695984480fe3973
Kimboo <kimboo@mail.ru>
2022-10-21 16:51:04 +0600

    123

--- test.js
+++ test.js
@@ -6,38 +6,41 @@ const User = require('./core/db/models/user')
 const jwt = require('jsonwebtoken')
 const dotenv = require('dotenv')
 const cors = require("cors")
+const isLoggedIn = require('./core/middleware/check_auth')
 
 dotenv.config()
-process.env.TOKEN_SECRET
 
 app.use(cors())
 app.use(express.json())
 
-const db = 'mongodb://sa:vbnJ456@192.168.31.135:27017/gosudesck?authSource=admin'
+mongoose.connect = require('./core/db/connection')
 
-mongoose.connect(db)
-        .then((res) => console.log('Connected DB'))
-        .catch((error) => console.log(error))
+let currentUser = undefined
 
-    
-        const auth = basicAuth({
-            authorizer: async (username, password, cb) => {
-              let currentUser = await User.findOne({login: username }).exec()
-              const userMatches = basicAuth.safeCompare(username, currentUser.login)
-              const passwordMatches = basicAuth.safeCompare(password, currentUser.password)
-              if (userMatches & passwordMatches)
-                return cb(null, true)
-              else
-                return cb(null, false)
-            },
-            authorizeAsync: true,
-          })
+const auth = basicAuth({
+  authorizer: async (username, password, cb) => {
+   currentUser = await User.findOne({login: username }).exec()
+   const userMatches = basicAuth.safeCompare(username, currentUser.login)
+   const passwordMatches = basicAuth.safeCompare(password, currentUser.password)
+   if (userMatches & passwordMatches)
+       return cb(null, true)
+    else
+       return cb(null, false)
+  },
+    authorizeAsync: true,
+  })
 
 app.get('/api/token', auth, async (request, responce) => {
     let params = {...request.body}
-    
-    //const token = generateAccessToken({ username: params.login });
-    responce.status(200).json(params)
+    currentUser.password = ''
+    const token = await jwt.sign({ user: currentUser }, process.env.SECRET, {
+      expiresIn: "24h"
+    });
+    responce.status(200).json({token})
+})
+
+app.post('/api/check', isLoggedIn, async (req, res) => {
+    res.status(200).json({...req.user})
 })
 
 app.get('*', (request, responce) => {
